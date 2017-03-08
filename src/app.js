f856edb74ae717ad8d1546f4b177a3fda793567d
TGrooves-208 <tgrooves.208@gmail.com>
2017-03-07 23:27:41 -0700

    React-Native Auth example using firebase

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAp-w4xxEjrofbd3rWwIKYPlKG1tceQ1mk',
+      authDomain: 'authentication-6e446.firebaseapp.com',
+      databaseURL: 'https://authentication-6e446.firebaseio.com',
+      storageBucket: 'authentication-6e446.appspot.com',
+      messagingSenderId: '172463422844'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
